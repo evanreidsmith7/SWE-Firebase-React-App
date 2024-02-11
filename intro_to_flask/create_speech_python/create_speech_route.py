9bda18fa54fb4aee4b29f9d72994e01c362b7989
Evan Smith <ers131@txstate.edu>
2024-02-11 00:35:33 -0600

    audio output without user input

--- intro_to_flask/create_speech_python/create_speech_route.py
+++ intro_to_flask/create_speech_python/create_speech_route.py
@@ -1,40 +1,44 @@
 import os
-import openai
 from openai import OpenAI
-import re #regular expressions module
-from markupsafe import escape #protects projects against injection attacks
+import re  # regular expressions module
+from markupsafe import escape  # protects projects against injection attacks
 from intro_to_flask import app
-import sys 
-sys.dont_write_bytecode = True
-from flask import render_template, request, Flask, Blueprint
+from flask import render_template, request, Flask, Blueprint, send_file
 from .create_speech_form import CreateSpeechForm
 from pathlib import Path
-import warnings
-warnings.filterwarnings("ignore", category=DeprecationWarning)
 
 create_speech_blueprint = Blueprint('create_speech', __name__)
 
-@create_speech_blueprint.route('/create_speech',methods=['GET', 'POST'])
-@app.route('/create_speech',methods=['GET', 'POST'])
+@create_speech_blueprint.route('/create_speech', methods=['GET', 'POST'])
+@app.route('/create_speech', methods=['GET', 'POST'])
 def create_speech():
   form = CreateSpeechForm(request.form)
-  
-  if request.method == 'POST':
-      if form.validate() == False:
-        return render_template('create_speech.html', form=form)
-      else:
-        # The following response code adapted from example on: 
-        # https://platform.openai.com/docs/api-reference/images
-        client = OpenAI()
 
-        speech_file_path = Path(__file__).parent / "speech.mp3"
-        response = client.audio.speech.create(
-            model="tts-1",
-            voice="alloy",
-            input="Today is a wonderful day to build something people love!"
-            )
-        speech = response.stream_to_file("speech.mp3")
-        return render_template('create_speech.html', speech_prompt=form.prompt.data, speech_response=speech, success=True)
-      
-  elif request.method == 'GET':
+  if request.method == 'POST':
+    if form.validate() == False:
       return render_template('create_speech.html', form=form)
+    else:
+      # The following response code adapted from example on:
+      # https://platform.openai.com/docs/api-reference/images
+      client = OpenAI()
+
+      speech_file_path = Path(__file__).parent / "speech.mp3"
+      response = client.audio.speech.create(
+        model="tts-1",
+        voice="alloy",
+        input="Today is a wonderful day to build something people love!"
+      )
+      with open(speech_file_path, "wb") as audio:
+        audio.write(response.content)
+
+      return render_template('create_speech.html',
+                   speech_prompt=form.prompt.data,
+                   speech_response=speech_file_path,
+                   success=True)
+
+  elif request.method == 'GET':
+    return render_template('create_speech.html', form=form)
+  
+@app.route('/audio')
+def serve_audio():
+    return send_file("create_speech_python/speech.mp3", mimetype='audio/mp3')
